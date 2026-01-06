07e4329f2745ea74dccf6235d497319978491e4c
carmen-tunta <carmen@tunta.com.bo>
2026-01-06 19:38:14 -0400

    Subiendo corregido cord1

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'http://localhost:5173', // Vite
+      'https://webhogar.onrender.com',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
